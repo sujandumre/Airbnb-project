1b97de3039dbee06ca1111397c6e3979fefcea33
sujan dumre <dumresujan12@gmail.com>
2025-12-31 15:36:18 +0545

    Initial commit - Wanderlust project

---
+++ app.js
@@ -0,0 +1,106 @@
+const express= require('express');
+const app= express();
+const mongoose = require('mongoose');
+const Listing = require('./models/listing.js');
+const path = require('path');
+const methodOverride = require('method-override');
+const ejsMate = require('ejs-mate');
+const wrapAsync = require('./utils/wrapAsync.js');
+const ExpressError = require('./utils/ExpressError.js');
+
+const MONGO_URL="mongodb://127.0.0.1:27017/wanderlust";
+main().then(()=>{
+  console.log("connected to db");
+}).catch((err)=>{
+  console.log(err);
+});
+async function main() {
+  await mongoose.connect(MONGO_URL);
+}
+
+app.set("view engine","ejs");
+app.set("views",path.join(__dirname,"/views"));
+app.use(express.urlencoded({extended:true}));
+app.use(methodOverride('_method'));
+app.engine('ejs', ejsMate);
+app.use(express.static(path.join(__dirname,'/public')));
+
+
+app.get('/',(req,res)=>{
+  res.send("hi there");
+});
+
+//index route to show all listings
+app.get('/listings',async(req,res)=>{
+  const allListings= await Listing.find({})
+  res.render("./listings/index.ejs",{allListings});
+});
+
+//new route
+app.get('/listings/new',(req,res)=>{
+  res.render("./listings/new.ejs");
+});
+
+//show route
+app.get('/listings/:id',wrapAsync(async(req,res)=>{
+  const {id} = req.params;
+  const listing = await Listing.findById(id);
+  res.render("./listings/show.ejs",{listing});
+}));
+
+//create route
+app.post('/listings',wrapAsync(async(req,res,next)=>{
+  
+    const newListing = new Listing(req.body.listing);
+    await newListing.save();
+    res.redirect("/listings");
+})
+);
+
+//edit route
+app.get('/listings/:id/edit',wrapAsync(async(req,res)=>{
+  const {id} = req.params;
+  const listing = await Listing.findById(id);
+  res.render("listings/edit.ejs",{listing});
+}));
+
+//update route
+app.put('/listings/:id',wrapAsync(async(req,res)=>{
+  const {id} = req.params;
+  await Listing.findByIdAndUpdate(id,{...req.body.listing});
+  res.redirect(`/listings/${id}`);
+}));
+
+//delete route
+app.delete('/listings/:id',wrapAsync(async(req,res)=>{
+  const {id} = req.params;
+  let deletedListing = await Listing.findByIdAndDelete(id);
+  console.log(deletedListing);
+  res.redirect('/listings');
+}));
+
+// app.get('/testlistings' ,async(req,res)=>{
+//   let sampleListing= new Listing({
+//     title:"Porche 911",
+//     description:"A beautiful sports car",
+//     price:150000,
+//     location:"Pokhara",
+//     country:"Nepal"
+//   });
+//   await sampleListing.save();
+//   res.send("Listing saved");
+//   console.log("successfully saved");
+// });
+
+app.use((req,res,next)=>{
+  next(new ExpressError(404,"page not found"));
+});
+
+
+app.use((err,req,res,next)=>{
+  let {statusCode=500,message="something went wrong"}=err;
+  res.status(statusCode).send(message);
+})
+app.listen(8000,()=>{
+  console.log("Server is running on port 8000");
+});
---
+++ models/listing.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const listingSchema = new Schema({
+  title:{
+    type:String,
+    required:true
+  },
+  description:String,
+  // image:{
+  // type:String,
+  // default:"https://unsplash.com/photos/red-ferrari-458-italia-on-road-during-daytime-YuUjJUBk6pY",
+  //   set:(v)=>v==="" ? 
+  //   "https://unsplash.com/photos/red-ferrari-458-italia-on-road-during-daytime-YuUjJUBk6pY":v,
+    image: {
+    filename: String,
+    url: String
+  },
+
+  price:Number,
+  location:String, 
+  country:String
+});
+
+const Listing = mongoose.model('Listing',listingSchema);
+module.exports = Listing;
