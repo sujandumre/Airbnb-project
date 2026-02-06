4a11dc4292603c48b7bb521d659fedf20182928b
sujan dumre <dumresujan12@gmail.com>
2026-02-06 23:01:39 +0545

    added

--- controller/listings.js
+++ controller/listings.js
@@ -1,4 +1,8 @@
 const Listing = require("../models/listing");
+const mbxGeocoding = require('@mapbox/mapbox-sdk/services/geocoding');
+const mapToken = process.env.MAP_TOKEN;
+const geocodingClient = mbxGeocoding({ accessToken: mapToken });
+
 
 module.exports.index = async (req, res) => {
   const allListings = await Listing.find({});
@@ -27,6 +31,13 @@ module.exports.showListing= async (req, res) => {
 };
 
 module.exports.createListing =async (req, res) => {
+  let response= await geocodingClient.forwardGeocode({
+  query: req.body.listing.location,
+  limit: 1,
+})
+.send();
+  
+
   let url = req.file.path;
   let filename = req.file.filename;
   console.log(url,'..',filename);
@@ -34,7 +45,9 @@ module.exports.createListing =async (req, res) => {
 
   newListing.owner=req.user._id;
   newListing.image = {url,filename};
-  await newListing.save();
+  newListing.geometry =response.body.features[0].geometry;
+  let savedListing= await newListing.save();
+  console.log(savedListing);
   req.flash('success', 'New listing created successfully!');
   return res.redirect('/listings');
 };
--- routes/listing.js
+++ routes/listing.js
@@ -1,4 +1,4 @@
-
+const {storage}=require('../cloudConfig.js');
 const express = require('express');
 const router = express.Router();
 const Listing = require('../models/listing.js');
@@ -7,7 +7,7 @@ const wrapAsync = require('../utils/wrapAsync.js');
 const {isLoggedIn, isOwner, validateListing}=require('../middleware.js');
 const listingController = require('../controller/listings.js');
 const multer  = require('multer');
-const {storage}=require('../cloudConfig.js');
+
 const upload = multer({storage });
 
 
